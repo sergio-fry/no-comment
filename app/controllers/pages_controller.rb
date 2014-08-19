077d9c20d05a310d48089b56a9a2ce065cb5521f
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-08-19 09:21:04 +0400

    форма добавления статьи

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,65 +1,15 @@
 class PagesController < Sinatra::Base
+  set :public_folder, File.join(App.root, 'public')
+  set :views, File.join(App.root, 'app/views')
+
   get "/" do
-    "No Comment"
+    erb "pages/index".to_sym, :locals => { }, :layout => :layout
   end
 
-  get %r{/c/(.+)} do |url|
-   
-    <<-HTML
-<html>
-<head>
-  <title>Комментарии для #{url}</title>
+  get %r{/comments/(.+)} do |url_without_protocol|
+    url = "http://" + url_without_protocol
+    page_id = url_without_protocol
 
-
-  <style>
-    #container { width: 100%;  max-width: 750px; text-align: left }
-  </style>
-</head>
-<body>
-  <center>
-    <div id="container">
-      <span style="font-size: 200%; font-weight: bold">No Comment?</span>
-      <br />
-      <em>Там, где комментариев нет</em>
-
-      <hr style="color: grey" />
-      <br />
-      <br />
-
-
-      <div class="article">
-      <a class="embedly-card" href="http://#{url}">#{url}</a>
-      <script>(function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b;d.async=true;d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}})(document);</script>
-      </div>
-      <br />
-
-      <div id="hypercomments_widget"></div>
-      <a href="http://hypercomments.com" class="hc-link" title="comments widget">комментарии на технологии HyperComments</a>
-    </div>
-  </center>
-
-  <script type="text/javascript">
-  _hcwp = window._hcwp || [];
-  var _hc_settings = {
-    widget:"Stream", widget_id:18390,
-    xid: "#{url}",
-    href: "#{url}"
-  };
-  _hcwp.push(_hc_settings);
-
-  (function() {
-   if("HC_LOAD_INIT" in window)return;
-   HC_LOAD_INIT = true;
-   var lang = (navigator.language || navigator.systemLanguage || navigator.userLanguage || "en").substr(0, 2).toLowerCase();
-   var hcc = document.createElement("script"); hcc.type = "text/javascript"; hcc.async = true;
-   hcc.src = ("https:" == document.location.protocol ? "https" : "http")+"://w.hypercomments.com/widget/hc/18390/"+lang+"/widget.js";
-   var s = document.getElementsByTagName("script")[0];
-   s.parentNode.insertBefore(hcc, s.nextSibling);
-   })();
-  </script>
-</body>
-</html>
-
-    HTML
+    erb "pages/comments".to_sym, :locals => { :url => url, :page_id => page_id }, :layout => :layout
   end
 end
---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+(function($) {
+  $("#add-page-form").on("submit", function() {
+    var url = $(this).find("#url").val().replace(/^\s+/, "").replace(/\s+$/, "");
+
+    if(url == "") {
+      $(this).find("#url").focus();
+    } else {
+      var domain = url.replace(/^https?:\/\//, "").split("/")[0];
+
+      var path = url.split(domain)[1].split("/").map(function(c) { return encodeURIComponent(c) }).join("/");
+
+      window.location.href = "/comments/" + domain + path;
+    }
+
+    return false;
+  });
+
+})(jQuery);
